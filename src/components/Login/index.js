c99f089cb8c464f6bda10e3a60fa3ecf24509fca
Lekkala Pavani <pavani.leela15@gmail.com>
2024-09-04 12:23:18 +0530

    updated

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,37 +1,141 @@
-
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import {jwtDecode} from "jwt-decode"; // Correct import for jwt-decode
 import "./index.css";
 
 const LoginForm = () => {
-    return (
-        <div className="login-container">
-            <h1 className="logo-image">RAPID RESOLVE</h1>
-            {/* <img src="https://res.cloudinary.com/dghlihlgi/image/upload/v1725085366/Preview_3_u4ch2z.png" alt="logo-here" className="logo-image"/> */}
-            <div className="image-con">
-                <img src="https://res.cloudinary.com/dghlihlgi/image/upload/v1725083306/Saly-3_ehkamc.jpg"
-                alt="Driverboy "className="image1"/>
-            </div>
-            <form className="login-form">
-                <h2>Login</h2>
-                <div className="input-group">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" id="username" name="username" required />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" required />
-                </div>
-                <div className="password-container">
-                    <p className="pass">forgot password</p>
-                </div>
-                <button type="submit" className="btn-login">Login</button>
-            </form>
-            <div>
-                <img src="https://res.cloudinary.com/dghlihlgi/image/upload/v1725083311/Saly-2_xlmkv4.jpg"
-                alt="delivery boy" className="image2"/>
-            </div>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [role, setRole] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [result, setResult] = useState({});
+  const navigate = useNavigate();
+
+  const handleInputChange = (event) => {
+    const { id, value } = event.target;
+    if (id === "email") {
+      setEmail(value);
+    } else if (id === "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch("http://localhost:9999/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        const jwtToken = data.token;
+        const decodedToken = jwtDecode(jwtToken);
+        const roles = decodedToken.roles;
+        const userRole = roles[0].slice(5); // Remove "ROLE_" from role string
+
+        Cookies.set("jwt_token", jwtToken);
+        Cookies.set("role", userRole);
+
+        const url =
+          userRole === "ADMIN"
+            ? `http://localhost:9999/api/volunteers/by-email/${email}`
+            : `http://localhost:9999/api/volunteers/email/${email}`;
+
+        const volunteerResponse = await fetch(url, {
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${jwtToken}`,
+          },
+          credentials: "include", // Ensures cookies are sent
+        });
+
+        if (volunteerResponse.ok) {
+          const data1 = await volunteerResponse.json();
+          setResult(data1);
+          navigate("/dashboard");
+        } else if (volunteerResponse.status === 404) {
+          if (userRole === "VOLUNTEER") {
+            navigate("/fill-profile", { state: { email } });
+          }
+        } else {
+          const errorData = await volunteerResponse.json();
+          setErrorMessage(
+            errorData || "An error occurred while checking user details."
+          );
+        }
+      } else {
+        const errorData = await response.json();
+        setErrorMessage(errorData.description || "Login failed");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      setErrorMessage("An error occurred during login.");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h1 className="logo-image">RAPID RESOLVE</h1>
+      <div className="image-con">
+        <img
+          src="https://res.cloudinary.com/dghlihlgi/image/upload/v1725083306/Saly-3_ehkamc.jpg"
+          alt="Driverboy"
+          className="image1"
+        />
+      </div>
+      <form onSubmit={handleSubmit} className="login-form">
+        <h2>Login</h2>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleInputChange}
+            required
+          />
         </div>
-    );
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        {/* <div className="form-group">
+          <label htmlFor="role">Role</label>
+          <select id="role" value={role} onChange={handleInputChange} required>
+            <option value="">Select</option>
+            <option value="ADMIN">ADMIN</option>
+            <option value="VOLUNTEER">VOLUNTEER</option>
+          </select>
+        </div> */}
+        {errorMessage && <p className="error">{errorMessage}</p>}
+        <button type="submit" className="btn-login">
+          Login
+        </button>
+      </form>
+      <div>
+        <img
+          src="https://res.cloudinary.com/dghlihlgi/image/upload/v1725083311/Saly-2_xlmkv4.jpg"
+          alt="delivery boy"
+          className="image2"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
--- src/components/RegisterVolunteer/index.js
+++ src/components/RegisterVolunteer/index.js
@@ -1,65 +1,133 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
 const RegisterVolunteer = () => {
-  const [selectedState, setSelectedState] = useState("");
-  const [selectedCity, setSelectedCity] = useState("");
+  const [selectedState, setSelectedState] = useState('');
+  const [selectedCity, setSelectedCity] = useState('');
   const [cities, setCities] = useState([]);
 
-  const navigate = useNavigate();
+  const location = useLocation();
+  const emailFromState = location.state?.email || '';
 
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    contactName: '',
+    address: '',
+    state: '',
+    volunteerCity: '',
+    age: '',
+    gender: '',
+    phoneNumber: '',
+    email: emailFromState,
+    skills: '',
+    // photoPath: ''
+  });
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  // List of states and corresponding cities
   const states = [
-    { name: "Maharashtra", cities: ["Mumbai", "Pune", "Nagpur", "Nashik"] },
-    { name: "Karnataka", cities: ["Bengaluru", "Mysuru", "Mangaluru", "Hubli"] },
-    { name: "Tamil Nadu", cities: ["Chennai", "Coimbatore", "Madurai", "Tiruchirappalli"] },
-    { name: "Uttar Pradesh", cities: ["Lucknow", "Kanpur", "Varanasi", "Agra"] },
-    { name: "West Bengal", cities: ["Kolkata", "Howrah", "Durgapur", "Siliguri"] },
-    { name: "Delhi", cities: ["New Delhi", "Dwarka", "Rohini", "Karol Bagh"] },
-    { name: "Gujarat", cities: ["Ahmedabad", "Surat", "Vadodara", "Rajkot"] },
-    { name: "Rajasthan", cities: ["Jaipur", "Jodhpur", "Udaipur", "Ajmer"] },
-    { name: "Telangana", cities: ["Hyderabad", "Warangal", "Nizamabad", "Karimnagar"] },
-    { name: "Kerala", cities: ["Thiruvananthapuram", "Kochi", "Kozhikode", "Kollam"] }
+    { name: 'Maharashtra', cities: ['Mumbai', 'Pune', 'Nagpur', 'Nashik'] },
+    { name: 'Karnataka', cities: ['Bengaluru', 'Mysuru', 'Mangaluru', 'Hubli'] },
+    { name: 'Tamil Nadu', cities: ['Chennai', 'Coimbatore', 'Madurai', 'Tiruchirappalli'] },
+    { name: 'Uttar Pradesh', cities: ['Lucknow', 'Kanpur', 'Varanasi', 'Agra'] },
+    { name: 'West Bengal', cities: ['Kolkata', 'Howrah', 'Durgapur', 'Siliguri'] },
+    { name: 'Delhi', cities: ['New Delhi', 'Dwarka', 'Rohini', 'Karol Bagh'] },
+    { name: 'Gujarat', cities: ['Ahmedabad', 'Surat', 'Vadodara', 'Rajkot'] },
+    { name: 'Rajasthan', cities: ['Jaipur', 'Jodhpur', 'Udaipur', 'Ajmer'] },
+    { name: 'Telangana', cities: ['Hyderabad', 'Warangal', 'Nizamabad', 'Karimnagar'] },
+    { name: 'Kerala', cities: ['Thiruvananthapuram', 'Kochi', 'Kozhikode', 'Kollam'] }
   ];
 
   const handleStateChange = (e) => {
     const selectedState = e.target.value;
     setSelectedState(selectedState);
 
-    const state = states.find(state => state.name === selectedState);
+    // Find the cities corresponding to the selected state
+    const state = states.find((state) => state.name === selectedState);
     setCities(state ? state.cities : []);
-    setSelectedCity(""); // Reset city selection when state changes
+    setSelectedCity(''); // Reset city selection when state changes
+
+    setFormData((prevData) => ({
+      ...prevData,
+      state: selectedState,
+      volunteerCity: '', // Reset city in formData
+    }));
   };
 
   const handleCityChange = (e) => {
-    setSelectedCity(e.target.value);
+    const selectedCity = e.target.value;
+    setSelectedCity(selectedCity);
+
+    setFormData((prevData) => ({
+      ...prevData,
+      volunteerCity: selectedCity,
+    }));
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const jwtToken = Cookies.get('jwt_token');
 
-    toast.success('Registration successful!', {
-      position: 'top-right',
-    });
+    try {
+      const response = await fetch('http://localhost:9999/api/volunteers/createVolunteer', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwtToken}`,
+        },
+        body: JSON.stringify(formData),
+      });
 
-    navigate('/login');
+      if (response.ok) {
+      
+        toast.success('Volunteer registered successfully!');
+        navigate('/dashboard');
+      } else {
+        const errorData = await response.json();
+        setErrorMessage(errorData.message);
+        toast.error('Registration failed: ' + errorData.message);
+      }
+    } catch (error) {
+      console.error('Error during registration:', error);
+      setErrorMessage('An error occurred during registration.');
+      toast.error('An error occurred during registration.');
+    }
   };
 
   return (
     <div className="register-container">
-      {/* <div className="logo">
-        <h1>Rapid Resolve</h1>
-        <img src="https://res.cloudinary.com/dghlihlgi/image/upload/v1725002683/Repidlogo_tibxzw.jpg" alt="Rapid Resolve Logo" className='logo-image'/>
-      </div> */}
       <div className="form-container">
         <h1>Register As Volunteer</h1>
         <p>We wish to help; you can register here</p>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Contact name" required />
-          <input type="text" placeholder="Address" />
+          <input
+            type="text"
+            name="contactName"
+            placeholder="Contact name"
+            value={formData.contactName}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="text"
+            name="address"
+            placeholder="Address"
+            value={formData.address}
+            onChange={handleChange}
+          />
           <div className="form-group">
             <label htmlFor="state">State</label>
             <select name="state" value={selectedState} onChange={handleStateChange} required>
@@ -71,7 +139,7 @@ const RegisterVolunteer = () => {
           </div>
           <div className="form-group">
             <label htmlFor="city">City</label>
-            <select name="city" value={selectedCity} onChange={handleCityChange} required>
+            <select name="volunteerCity" value={selectedCity} onChange={handleCityChange} required>
               <option value="">Select a city</option>
               {cities.map((city, index) => (
                 <option key={index} value={city}>{city}</option>
@@ -79,23 +147,61 @@ const RegisterVolunteer = () => {
             </select>
           </div>
           <div className="age-postcode">
-            <input type="number" placeholder="Age" required />
-            <input type="text" placeholder="Gender" required />
+            <input
+              type="number"
+              name="age"
+              placeholder="Age"
+              value={formData.age}
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="text"
+              name="gender"
+              placeholder="Gender"
+              value={formData.gender}
+              onChange={handleChange}
+              required
+            />
           </div>
-          <input type="tel" placeholder="Contact Phone" required />
-          <input type="email" placeholder="E-mail" />
-          <div className="age-postcode">
-            <input type="password" placeholder="Password" required />
-            <input type="password" placeholder="Confirm password" required />
-          </div>
-          <input type="text" placeholder="Skills/Training" />
-          <div className="upload">
+          <input
+            type="tel"
+            name="phoneNumber"
+            placeholder="Contact Phone"
+            value={formData.phoneNumber}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="E-mail"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="skills"
+            placeholder="Skills/Training"
+            value={formData.skills}
+            onChange={handleChange}
+          />
+          {/* <div className="upload">
             <label>Upload a Photo</label>
-            <input id="file-upload" type="file" />
+            <input
+              id="file-upload"
+              type="file"
+              name="photoPath"
+              onChange={(e) => setFormData((prevData) => ({
+                ...prevData,
+                photoPath: e.target.files[0],
+              }))}
+            />
             <small>Attach file. File size of your documents should not exceed 10MB</small>
-          </div>
+          </div> */}
           <button type="submit">SUBMIT</button>
         </form>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
       </div>
       <ToastContainer
         position="top-right"
@@ -110,6 +216,6 @@ const RegisterVolunteer = () => {
       />
     </div>
   );
-}
+};
 
 export default RegisterVolunteer;
